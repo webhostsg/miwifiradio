6097f70d8f4fe7a041ab01ce098aa82e0e8732a5
webhostsg <59085206+webhostsg@users.noreply.github.com>
2021-03-29 19:42:52 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 if (!defined("_KATE_MAIN")) die("You have no access to this file");
-    $dbhost ="192.168.1.132";
-    $dbuname ="dev";
-    $dbpass ="dev";
-    $dbname ="miradio";
-    $global_path ="/var/www/html/miwifiradio";
+    $dbhost = getenv('MYSQL_HOST');
+    $dbuname = getenv('MYSQL_USER');
+    $dbpass = getenv('MYSQL_PASSWORD');
+    $dbname = getenv('MYSQL_DATABASE');
+    $global_path = getenv('MIR_WWW_PATH');
     $ffbin = "ffmpeg";
-    $domain ="192.168.1.132";
+    $domain = getenv('MIR_DOMAIN_NAME');
     // Sync with Master - send your playlist to masterserver
     $sync=0;
     $master_host="ximiraga.ru";
